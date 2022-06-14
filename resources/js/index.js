1f77633176876465570c29486793d90c3b0f211e
erickhansx <50700379+erickhansx@users.noreply.github.com>
2022-06-14 13:26:32 -0500

    Add script to the list in menu to redirect to section when clicked

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,11 +1,25 @@
-const mobileMenu = document.querySelector(".mobile-menu");
-const burguer = document.querySelector(".hamburguer-menu");
-const xmenu = document.querySelector(".x-mobile");
-burguer.addEventListener("click", show);
-xmenu.addEventListener("click", close);
+const mobileMenu = document.querySelector('.mobile-menu');
+const burguer = document.querySelector('.hamburguer-menu');
+const xmenu = document.querySelector('.x-mobile');
+const menulist = document.querySelector('.link-1');
+const menulist2 = document.querySelector('.link-2');
+const menulist3 = document.querySelector('.link-3');
+
+function close1() {
+  mobileMenu.style.top = '-100%';
+}
+
 function close() {
-  mobileMenu.style.top = "-100%";
+  mobileMenu.style.top = '-100%';
 }
+
 function show() {
-  mobileMenu.style.display = "block";
+  mobileMenu.style.display = 'flex';
+  mobileMenu.style.top = '0';
 }
+
+burguer.addEventListener('click', show);
+xmenu.addEventListener('click', close);
+menulist.addEventListener('click', close1);
+menulist2.addEventListener('click', close1);
+menulist3.addEventListener('click', close1);
