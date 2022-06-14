36e4705ad903f2b37b9758c46f837e5911b955dc
wdavidcch <david.13.2121@gmail.com>
2022-06-14 13:13:36 -0400

    Add JS events

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,11 @@
-
+const mobileMenu = document.querySelector(".mobile-menu");
+const burguer = document.querySelector(".hamburguer-menu");
+const xmenu = document.querySelector(".x-mobile");
+burguer.addEventListener("click", show);
+xmenu.addEventListener("click", close);
+function close() {
+  mobileMenu.style.top = "-100%";
+}
+function show() {
+  mobileMenu.style.display = "block";
+}
