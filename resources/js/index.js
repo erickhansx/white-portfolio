e55b4f5e9fea00c6ba4e7177ac4f900986f15b35
erickhansx <50700379+erickhansx@users.noreply.github.com>
2022-06-15 10:13:48 -0500

    Create works objects

--- resources/js/index.js
+++ resources/js/index.js
@@ -23,3 +23,44 @@ xmenu.addEventListener('click', close);
 menulist.addEventListener('click', close1);
 menulist2.addEventListener('click', close1);
 menulist3.addEventListener('click', close1);
+
+// Modal Window
+
+let worksInfo = [
+  {
+    title: 'Tonic',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    Paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['html', 'css', 'javaScript'],
+    button: 'See Project',
+    image: './resources/img/project1.png',
+  },
+  {
+    title: 'Multi-Post Stories',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    Paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['html', 'css', 'javaScript'],
+    button: 'See Project',
+    image: './resources/img/project2.png',
+  },
+  {
+    title: 'Tonic',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    Paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['html', 'css', 'javaScript'],
+    button: 'See Project',
+    image: './resources/img/project3.png',
+  },
+  {
+    title: 'Multi-Post Stories',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    Paragraph:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    languages: ['html', 'css', 'javaScript'],
+    button: 'See Project',
+    image: './resources/img/project4.png',
+  },
+];
